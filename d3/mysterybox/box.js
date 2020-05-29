ee4d52f6812289531b2ca1e97be3c8ff17743a42
Lawrence McDaniel <lpm0073@gmail.com>
2020-05-29 14:12:58 -0500

    function draft #2 of logo cube

--- d3/mysterybox/box.js
+++ d3/mysterybox/box.js
@@ -2,39 +2,37 @@
 https://bl.ocks.org/veltman/4c989172ac2f820b0b7267c53cb96975
 */
 var user = "tophtucker";
-
 var api = "https://api.lawrencemcdaniel.com/wp-json/wp/v2/posts?categories=43&_embed&per_page=100";
+var logos = [];
 
 d3.json(api, function(err, posts) {
 
-  posts = posts.map(function(post) {
-    retval = post._embedded["wp:featuredmedia"][0].source_url;
-    return retval;
+  logos = posts.map(function(post) {
+    return post._embedded["wp:featuredmedia"][0].source_url;
   });
 
-  d3.shuffle(posts);
-
-  var sides = d3.selectAll(".logo");
-  console.log(sides);
-
-  sides.transition()
-    .delay(function(d,i) {
-      return i * 2500;
-    })
-    .each("end", mystery);
-
-  function mystery() {
-
-    var next = posts[posts.push(posts.shift()) - 1];
-
-    var logo_div = d3.select(this);
-
-    background_image = "url('" + next + "')";
-    console.log(background_image);
-
-    logo_div.style("background-image", background_image);
-    console.log(logo_div);
-  }
-
+  d3.shuffle(logos);
 
 });
+
+function random_logo() {
+  return logos[Math.floor(Math.random() * logos.length)];
+}
+
+function repaint() {
+
+  d3.selectAll(".logo")
+    .each(function() {
+
+      var side = d3.select(this);
+      setTimeout(function() {
+          side.style("background-image", "url('" + random_logo() + "')");
+        }, 5000*Math.random());   
+
+
+    });
+
+}
+
+  setTimeout(repaint, 1000);   
+
